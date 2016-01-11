764e93fa57ae1b9c0bf6fb974511f8d584d2d9ea
Gkxd <gkxd@users.noreply.github.com>
2016-01-11 11:23:05 -0500

    Added automatically resizing quad to be full screen

---
+++ Assets/Useful Scripts/Miscellaneous/SetFullscreenOrthographicQuad.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UsefulThings {
+    [ExecuteInEditMode]
+    public class SetFullscreenOrthographicQuad : MonoBehaviour {
+        public Camera camera;
+        void Start() {
+            float height = camera.orthographicSize * 2;
+            float width = height * camera.aspect;
+
+            transform.localScale = new Vector3(width, height, 1);
+        }
+    }
+}
