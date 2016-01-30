d12765fbee6227e711eff129a2dd41fb81497da4
Gkxd <gkxd@users.noreply.github.com>
2016-01-29 22:57:00 -0500

    Added destroying GameObjects

---
+++ Assets/Useful Scripts/Miscellaneous/DestroyGameObjectAfterSeconds.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyGameObjectAfterSeconds : MonoBehaviour {
+    public float seconds;
+
+    void Start() {
+        Destroy(gameObject, seconds);
+    }
+}
