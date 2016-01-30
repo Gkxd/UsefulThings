87bf5d3c44742a11a140a507844c00dbc2e7ddc2
Gkxd <gkxd@users.noreply.github.com>
2016-01-30 02:27:05 -0500

    Added audio listener output

---
+++ Assets/Useful Scripts/Miscellaneous/AudioListenerOutput.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UsefulThings {
+    public class AudioListenerOutput : MonoBehaviour {
+
+        public static float[] soundWave;
+        public static float[] frequencySpectrum;
+
+        [Range(1, 13)]
+        public int logNumFrequencySamples;
+
+        void Start() {
+            frequencySpectrum = new float[1 << logNumFrequencySamples];
+        }
+
+        void OnAudioFilterRead(float[] data, int channels) {
+            soundWave = data;
+            AudioListener.GetSpectrumData(frequencySpectrum, 0, FFTWindow.Rectangular);
+        }
+    }
+}
