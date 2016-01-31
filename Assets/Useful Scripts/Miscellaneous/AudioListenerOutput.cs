f5262366cf581a584d14b24e8a82316bf66cc824
Gkxd <gkxd@users.noreply.github.com>
2016-01-30 23:16:27 -0500

    Removed frequency spectrum

--- Assets/Useful Scripts/Miscellaneous/AudioListenerOutput.cs
+++ Assets/Useful Scripts/Miscellaneous/AudioListenerOutput.cs
@@ -3,20 +3,7 @@ using System.Collections;
 
 namespace UsefulThings {
     public class AudioListenerOutput : MonoBehaviour {
-
         public static float[] soundWave;
-        public static float[] frequencySpectrum;
-
-        [Range(1, 13)]
-        public int logNumFrequencySamples;
-
-        void Start() {
-            frequencySpectrum = new float[1 << logNumFrequencySamples];
-        }
-
-        void Update() {
-            AudioListener.GetSpectrumData(frequencySpectrum, 0, FFTWindow.Rectangular);
-        }
 
         void OnAudioFilterRead(float[] data, int channels) {
             soundWave = data;
