df7728522e0bd7dae7fb132bed14462aacf57f8a
Gkxd <gkxd@users.noreply.github.com>
2016-01-30 23:16:46 -0500

    Added screenshot component

---
+++ Assets/Useful Scripts/Miscellaneous/TakeScreenshot.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UsefulThings {
+    public class TakeScreenshot : MonoBehaviour {
+
+        public KeyCode screenshotKey;
+
+        void Awake() {
+            DontDestroyOnLoad(gameObject);
+        }
+
+        void Update() {
+            if (Input.GetKeyDown(screenshotKey)) {
+                Application.CaptureScreenshot(Application.dataPath + "/../Screenshots/screenshot_" + System.DateTime.Now.ToString("yyyyMMddHHmmssffff") + ".png");
+            }
+        }
+    }
+}
