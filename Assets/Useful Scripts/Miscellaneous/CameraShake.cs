c25376a7d8afebf3c920eade740e5394e3e81c9c
Gkxd <gkxd@users.noreply.github.com>
2016-02-12 23:59:57 -0500

    Added camera shake script

---
+++ Assets/Useful Scripts/Miscellaneous/CameraShake.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UsefulThings {
+    public class CameraShake : MonoBehaviour {
+        // *** This script doesn't actually shake the camera! ***
+
+        // To actually shake the camera, offset the camera's position with CameraShakeOffset
+        // in another script, and call CameraShake.ShakeCamera() from anywhere whenever you
+        // want to shake the camera. (See CameraShakeTest.cs for example usage.)
+
+        private static CameraShake instance;
+
+        public static Vector3 CameraShakeOffset;
+
+
+        public bool shakeZ;
+
+        private float timeBetweenShakes = 0.05f;
+        private float cameraShakeMagnitude;
+        private float cameraShakeDuration;
+        private float cameraShakeTimer;
+
+        private Vector3 cameraShakeTargetOffset;
+
+        void Start() {
+            if (instance == null) {
+                instance = this;
+            }
+        }
+
+        void Update() {
+            if (cameraShakeDuration > 0) {
+                cameraShakeTimer += Time.deltaTime;
+
+                if (cameraShakeTimer > timeBetweenShakes) {
+                    cameraShakeTimer -= timeBetweenShakes;
+
+                    cameraShakeTargetOffset = Random.insideUnitSphere * cameraShakeMagnitude;
+                    if (!shakeZ) {
+                        cameraShakeTargetOffset.z = 0;
+                    }
+                }
+
+                cameraShakeDuration -= Time.deltaTime;
+                cameraShakeMagnitude = Mathf.Lerp(cameraShakeMagnitude, 0, Time.deltaTime);
+            }
+            else if (cameraShakeDuration < 0) {
+                cameraShakeDuration = 0;
+                cameraShakeMagnitude = 0;
+                cameraShakeTimer = 0;
+                cameraShakeTargetOffset = Vector3.zero;
+            }
+
+            CameraShakeOffset = Vector3.Lerp(CameraShakeOffset, cameraShakeTargetOffset, 15 * Time.deltaTime);
+        }
+
+        public static void ShakeCamera(float magnitude, float duration) {
+            ShakeCamera(magnitude, duration, 0.05f);
+        }
+
+        public static void ShakeCamera(float magnitude, float duration, float timeBetweenShakes) {
+            if (instance) {
+                instance.cameraShakeMagnitude += magnitude;
+                instance.cameraShakeDuration += duration;
+                instance.timeBetweenShakes = timeBetweenShakes;
+            }
+        }
+    }
+}
---
+++ Assets/Useful Scripts/Miscellaneous/CameraShakeTest.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UsefulThings {
+    public class CameraShakeTest : MonoBehaviour {
+
+        Vector3 originalPosition;
+
+        void Start() {
+            originalPosition = transform.localPosition;
+        }
+
+        void Update() {
+            transform.localPosition = originalPosition + CameraShake.CameraShakeOffset;
+
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                CameraShake.ShakeCamera(1, 0.2f);
+            }
+        }
+    }
+}
