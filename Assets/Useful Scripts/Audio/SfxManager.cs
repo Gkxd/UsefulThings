f4a8f49a55016c56e22f47a7106d183d3acc6023
Gkxd <gkxd@users.noreply.github.com>
2016-09-26 14:04:55 -0400

    Added loop playing to SfxManager

--- Assets/Useful Scripts/Audio/SfxManager.cs
+++ Assets/Useful Scripts/Audio/SfxManager.cs
@@ -13,15 +13,20 @@ namespace UsefulThings
         private static SfxManager instance;
 
         public AudioClip[] sfxBank;
+        public AudioClip[] loopBank;
+
         private GameObject[] sfxObjects;
+        private GameObject[] loopObjects;
 
         private bool[] sfxLocks;
+        private bool[] loopLocks;
 
         void Start()
         {
             instance = this;
 
             sfxObjects = new GameObject[sfxBank.Length];
+            loopObjects = new GameObject[loopBank.Length];
 
             for (int i = 0; i < sfxBank.Length; i++)
             {
@@ -36,7 +41,22 @@ namespace UsefulThings
                 }
             }
 
+            for (int i = 0; i < loopBank.Length; i++)
+            {
+                if (loopBank[i])
+                {
+                    GameObject loop = new GameObject(loopBank[i].name);
+                    loop.SetActive(false);
+                    loop.transform.parent = transform;
+                    AudioSource audioSource = loop.AddComponent<AudioSource>();
+                    audioSource.loop = true;
+                    audioSource.clip = loopBank[i];
+                    loopObjects[i] = loop;
+                }
+            }
+
             sfxLocks = new bool[sfxBank.Length];
+            loopLocks = new bool[loopBank.Length];
         }
 
         /// <summary>
@@ -84,6 +104,39 @@ namespace UsefulThings
         {
             yield return new WaitForSeconds(time);
             sfxLocks[sfxId] = false;
+        }
+
+        /// <summary>
+        /// Plays a looped sound effect. Only one of each loop can be played at a time.
+        /// </summary>
+        /// <param name="loopId">The index of the sound effect in the loopBank array.</param>
+        /// <param name="volume">The volume to play the sound effect at. (Default 1) </param>
+        public static void PlayLoop(int loopId, float volume = 1)
+        {
+            if (instance) instance.playLoop(loopId, volume);
+        }
+
+        private void playLoop(int loopId, float volume)
+        {
+            if (loopObjects[loopId])
+            {
+                loopObjects[loopId].SetActive(true);
+                loopObjects[loopId].GetComponent<AudioSource>().volume = volume;
+            }
+        }
+
+        /// <summary>
+        /// Stop playing a looped sound effect.
+        /// </summary>
+        /// <param name="loopId">The index of the sound effect in the loopBank array.</param>
+        public static void StopLoop(int loopId)
+        {
+            if (instance) instance.stopLoop(loopId);
+        }
+
+        private void stopLoop(int loopId)
+        {
+            if (loopObjects[loopId]) loopObjects[loopId].SetActive(false);
         }
     }
 }
