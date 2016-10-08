63836284f8a6ff2c9b7d67b6a75acf3473709f27
Gkxd <gkxd@users.noreply.github.com>
2016-10-08 14:53:32 -0400

    Adding bezier splines with uniform distance sampling

---
+++ Assets/Useful Scripts/_Core/BezierSpline.cs
@@ -0,0 +1,243 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace UsefulThings
+{
+    public class BezierSpline : MonoBehaviour
+    {
+
+        public List<Vector3> points;
+
+        // Used for uniform curve evaluation
+        private const int approximationResolution = 1000;
+        [SerializeField]
+        private List<Vector3> curveApproximation;
+        [SerializeField]
+        private List<float> cumulativeDistances;
+        [SerializeField]
+        private float totalDistance;
+
+        public Vector3 Evaluate(float t, bool debugLog = false)
+        {
+            int segment = (int)t;
+            int index = 3 * segment;
+
+            float t0 = t - segment;
+            float t1 = 1 - t0;
+
+            Vector3 p0 = points[index++];
+            Vector3 p1 = points[index++];
+            Vector3 p2 = points[index++];
+            Vector3 p3 = points[index++];
+
+            if (debugLog)
+            {
+                Debug.Log(p0 + " " + p1 + " " + p2 + " " + p3 + " " + t + " " + t0 + " " + t1);
+            }
+
+            float a = t1 * t1 * t1;
+            float b = t1 * t1 * t0 * 3;
+            float c = t1 * t0 * t0 * 3;
+            float d = t0 * t0 * t0;
+
+            return a * p0 + b * p1 + c * p2 + d * p3;
+        }
+
+        public Vector3 Derivative(float t)
+        {
+            int segment = (int)t;
+            int index = 3 * segment;
+
+            float t0 = t - segment;
+            float t1 = 1 - t0;
+
+            Vector3 p0 = points[index++];
+            Vector3 p1 = points[index++];
+            Vector3 p2 = points[index++];
+            Vector3 p3 = points[index++];
+
+            float a = t1 * t1;
+            float b = t1 * t0;
+            float c = t0 * t0;
+
+            return a * (p1 - p0) + b * (p2 - p1) + c * (p3 - p2);
+        }
+
+        /// <summary>
+        /// Recomputes the curve approximation used for uniform sampling. Avoid calling this often, since this is slow.
+        /// </summary>
+        public void RecomputeApproximateCurve()
+        {
+            curveApproximation = new List<Vector3>();
+            cumulativeDistances = new List<float>();
+
+            int numSegments = points.Count / 3;
+            Vector3 prevPoint = Vector3.zero;
+            float cumulativeDistance = 0;
+
+            for (int i = 0; i < approximationResolution; i++)
+            {
+                float t = i * ((float)numSegments / approximationResolution);
+                Vector3 p = Evaluate(t);
+                curveApproximation.Add(p);
+
+                if (i > 0)
+                {
+                    cumulativeDistance += (p - prevPoint).magnitude;
+                }
+                cumulativeDistances.Add(cumulativeDistance);
+
+                prevPoint = p;
+            }
+
+            totalDistance = cumulativeDistance + (prevPoint - points[points.Count - 1]).magnitude;
+        }
+
+        public Vector3 EvaluateUniform(float t)
+        {
+            int numSegments = points.Count / 3;
+            float distance = t / numSegments * totalDistance;
+            int index = cumulativeDistances.BinarySearch(distance);
+
+            if (index < 0)
+            {
+                index = ~index - 1;
+
+                if (index < cumulativeDistances.Count - 1)
+                {
+                    float distancePercent = (distance - cumulativeDistances[index]) / (cumulativeDistances[index + 1] - cumulativeDistances[index]);
+                    return Vector3.Lerp(curveApproximation[index], curveApproximation[index + 1], distancePercent);
+                }
+                else
+                {
+                    float distancePercent = (distance - cumulativeDistances[index]) / (totalDistance - cumulativeDistances[index]);
+                    return Vector3.Lerp(curveApproximation[index], points[points.Count - 1], distancePercent);
+                }
+            }
+            else
+            {
+                return curveApproximation[index];
+            }
+        }
+        
+        public Vector3 DerivativeUniform(float t)
+        {
+            int numSegments = points.Count / 3;
+            float distance = t / numSegments * totalDistance;
+            int index = cumulativeDistances.BinarySearch(distance);
+
+            if (index < 0)
+            {
+                index = ~index - 1;
+            }
+
+            if (index < cumulativeDistances.Count - 1)
+            {
+                return curveApproximation[index + 1] - curveApproximation[index];
+            }
+            else
+            {
+                return points[points.Count - 1] - curveApproximation[index];
+            }
+        }
+
+        // EDITOR ONLY
+#if UNITY_EDITOR
+
+        public enum ContinuityType
+        {
+            NONE, TANGENTS, TANGENT_MAGNITUDES
+        }
+        public Color curveColor = Color.green;
+        public Color tangentColor = Color.white;
+        public Color uniformSamplingColor = Color.blue;
+        public List<ContinuityType> continuity;
+
+        void Reset()
+        {
+            points = new List<Vector3>();
+            points.Add(new Vector3(0, 0, 1));
+            points.Add(new Vector3(0, 0, 2));
+            points.Add(new Vector3(0, 0, 3));
+            points.Add(new Vector3(0, 0, 4));
+
+            continuity = new List<ContinuityType>();
+
+            continuity.Add(ContinuityType.NONE);
+            continuity.Add(ContinuityType.NONE);
+            continuity.Add(ContinuityType.NONE);
+            continuity.Add(ContinuityType.NONE);
+
+            RecomputeApproximateCurve();
+        }
+
+        public void AddSegment()
+        {
+            Vector3 lastPoint = points[points.Count - 1];
+            points.Add(lastPoint + new Vector3(0, 0, 1));
+            points.Add(lastPoint + new Vector3(0, 0, 2));
+            points.Add(lastPoint + new Vector3(0, 0, 3));
+
+            continuity.Add(ContinuityType.NONE);
+            continuity.Add(ContinuityType.NONE);
+            continuity.Add(ContinuityType.NONE);
+
+            RecomputeApproximateCurve();
+        }
+
+        public void RemoveSegment()
+        {
+            points.RemoveRange(points.Count - 3, 3);
+
+            RecomputeApproximateCurve();
+        }
+
+        public void SetControlPoint(int index, Vector3 value)
+        {
+            Vector3 oldPosition = points[index];
+            Vector3 offset = value - oldPosition;
+
+            if (index % 3 == 0)
+            {
+                points[index] = value;
+                if (index + 1 < points.Count)
+                {
+                    points[index + 1] += offset;
+                }
+                if (index - 1 >= 0)
+                {
+                    points[index - 1] += offset;
+                }
+            }
+            else
+            {
+                int cornerIndex = index % 3 == 1 ? index - 1 : index + 1;
+                int oppositeIndex = index % 3 == 1 ? index - 2 : index + 2;
+
+                Vector3 fromCorner = value - points[cornerIndex];
+
+                points[index] = value;
+                switch (continuity[cornerIndex])
+                {
+                    case ContinuityType.NONE:
+                        break; // Do Nothing
+                    case ContinuityType.TANGENTS:
+                        if (oppositeIndex >= 0 && oppositeIndex < points.Count)
+                        {
+                            float magnitude = (points[oppositeIndex] - points[cornerIndex]).magnitude;
+                            points[oppositeIndex] = points[cornerIndex] - fromCorner.normalized * magnitude;
+                        }
+                        break;
+                    case ContinuityType.TANGENT_MAGNITUDES:
+                        if (oppositeIndex >= 0 && oppositeIndex < points.Count)
+                        {
+                            points[oppositeIndex] = points[cornerIndex] - fromCorner;
+                        }
+                        break;
+                }
+            }
+        }
+#endif
+    }
+}
---
+++ Assets/Useful Scripts/_Core/Editor/BezierSplineDrawer.cs
@@ -0,0 +1,174 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+namespace UsefulThings
+{
+    [CustomEditor(typeof(BezierSpline))]
+    public class BezierSplineDrawer : Editor
+    {
+        private int selectedIndex = -1;
+
+        private void OnSceneGUI()
+        {
+            BezierSpline curve = (BezierSpline)target;
+
+            Transform t = curve.transform;
+            Quaternion r = Tools.pivotRotation == PivotRotation.Local ? t.rotation : Quaternion.identity;
+
+            for (int i = 0; i < curve.points.Count; i++)
+            {
+                Vector3 p = t.TransformPoint(curve.points[i]);
+
+                Handles.color = Color.white;
+
+                float size = HandleUtility.GetHandleSize(p);
+                if (Handles.Button(p, r, size * 0.04f, size * 0.06f, Handles.DotCap))
+                {
+                    selectedIndex = i;
+                    Repaint();
+                }
+
+                if (selectedIndex == i)
+                {
+                    EditorGUI.BeginChangeCheck();
+                    p = Handles.DoPositionHandle(p, r);
+                    if (EditorGUI.EndChangeCheck())
+                    {
+                        Undo.RecordObject(curve, "Move Point");
+                        curve.SetControlPoint(i, t.InverseTransformPoint(p));
+                        EditorUtility.SetDirty(curve);
+                    }
+                }
+            }
+
+            Handles.color = curve.tangentColor;
+            for (int i = 0; i < curve.points.Count - 1; i++)
+            {
+                if (i % 3 == 1) continue;
+
+                Vector3 p0 = t.TransformPoint(curve.points[i]);
+                Vector3 p1 = t.TransformPoint(curve.points[i + 1]);
+                Handles.DrawLine(p0, p1);
+            }
+
+            Handles.color = curve.curveColor;
+            for (int i = 0; i < curve.points.Count / 3; i++)
+            {
+                Vector3 p0 = t.TransformPoint(curve.points[3 * i]);
+                Vector3 p1 = t.TransformPoint(curve.points[3 * i + 1]);
+                Vector3 p2 = t.TransformPoint(curve.points[3 * i + 2]);
+                Vector3 p3 = t.TransformPoint(curve.points[3 * i + 3]);
+
+                Handles.DrawBezier(p0, p3, p1, p2, curve.curveColor, null, 2f);
+            }
+
+            Handles.color = curve.uniformSamplingColor;
+            if (Camera.current)
+            {
+                Vector3 up = Camera.current.transform.up;
+                Vector3 right = Camera.current.transform.right;
+
+                for (float i = 0; i < curve.points.Count / 3; i += 0.02f)
+                {
+                    Vector3 p0 = t.TransformPoint(curve.EvaluateUniform(i));
+                    float size = HandleUtility.GetHandleSize(p0) * 0.02f;
+
+                    Vector3[] rect = new Vector3[] { p0 - right * size, p0 - up * size, p0 + right * size, p0 + up * size };
+
+                    Handles.DrawSolidRectangleWithOutline(rect, curve.uniformSamplingColor, curve.uniformSamplingColor);
+                }
+            }
+        }
+
+        public override void OnInspectorGUI()
+        {
+            //base.OnInspectorGUI();
+            BezierSpline curve = (BezierSpline)target;
+            serializedObject.Update();
+            /*
+            EditorGUILayout.PropertyField(points, new GUIContent("Segments"));
+            if (points.isExpanded)
+            {
+                EditorGUI.indentLevel += 1;
+                for (int i = 0; i < points.arraySize; i++)
+                {
+                    EditorGUILayout.BeginHorizontal();
+                    EditorGUILayout.LabelField(new GUIContent("P" + i), GUILayout.Width(55));
+                    EditorGUILayout.PropertyField(points.GetArrayElementAtIndex(i), GUIContent.none, GUILayout.Width(250));
+                    EditorGUILayout.EndHorizontal();
+                }
+                EditorGUI.indentLevel -= 1;
+            }
+            */
+
+            EditorGUILayout.LabelField(new GUIContent("Selected Point"), EditorStyles.boldLabel);
+            SerializedProperty points = serializedObject.FindProperty("points");
+            SerializedProperty continuity = serializedObject.FindProperty("continuity");
+
+            if (selectedIndex > -1 && selectedIndex < curve.points.Count)
+            {
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.LabelField(new GUIContent("P" + selectedIndex), GUILayout.Width(35));
+                EditorGUILayout.PropertyField(points.GetArrayElementAtIndex(selectedIndex), GUIContent.none);
+                EditorGUILayout.EndHorizontal();
+
+                int cornerIndex = selectedIndex;
+                if (selectedIndex % 3 == 1)
+                {
+                    cornerIndex = selectedIndex - 1;
+                }
+                else if (selectedIndex % 3 == 2)
+                {
+                    cornerIndex = selectedIndex + 1;
+                }
+
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.LabelField(new GUIContent("Continuity Type:"), GUILayout.Width(100));
+                EditorGUILayout.PropertyField(continuity.GetArrayElementAtIndex(cornerIndex), GUIContent.none);
+                EditorGUILayout.EndHorizontal();
+            }
+            else
+            {
+                EditorGUILayout.LabelField(new GUIContent("No Point Selected"));
+            }
+
+            EditorGUILayout.Separator();
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField(new GUIContent("Number of Segments: "), EditorStyles.boldLabel, GUILayout.Width(150));
+            EditorGUILayout.LabelField(new GUIContent("" + curve.points.Count / 3), GUILayout.Width(25));
+            if (GUILayout.Button("+", EditorStyles.miniButtonLeft))
+            {
+                Undo.RecordObject(curve, "Add Segment");
+                curve.AddSegment();
+                EditorUtility.SetDirty(curve);
+            }
+            if (GUILayout.Button("-", EditorStyles.miniButtonRight))
+            {
+                if (curve.points.Count >= 7)
+                {
+                    Undo.RecordObject(curve, "Remove Segment");
+                    curve.RemoveSegment();
+                    EditorUtility.SetDirty(curve);
+                }
+            }
+            EditorGUILayout.EndHorizontal();
+
+            if (GUILayout.Button("Recompute Curve Approximation"))
+            {
+                Undo.RecordObject(curve, "Recompute Curve Approximation");
+                curve.RecomputeApproximateCurve();
+                EditorUtility.SetDirty(curve);
+            }
+
+
+            EditorGUILayout.Separator();
+            EditorGUILayout.LabelField(new GUIContent("Display Options"), EditorStyles.boldLabel);
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("curveColor"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("tangentColor"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("uniformSamplingColor"));
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
